4bc0bf3a0b942ce7af5dea4bccfd17bab8eae5f5
Bo Yuan <boyuxa@syntronic.com>
2024-01-29 20:13:00 -0500

    removed commented line.

--- app/api-services/open-weather-service.ts
+++ app/api-services/open-weather-service.ts
@@ -6,16 +6,11 @@ const REDIS_HOST = process.env.REDIS_HOST
 const REDIS_KEY = process.env.REDIS_KEY
 
 // Create a Redis client
-// REDIS_HOST="myRedisInstance15813d52.redis.cache.windows.net"
-// REDIS_KEY="KRVmBdHeRsQwKFvfkiJV8uQDde9oNYJK8AzCaJRfIRo="
 const redisClient = new Redis({
   host: REDIS_HOST,
-  //  "myRedisInstance15813d52.redis.cache.windows.net", // Replace with your Azure Redis Cache name
   port: 6380,
   password: REDIS_KEY,
-  //  "KRVmBdHeRsQwKFvfkiJV8uQDde9oNYJK8AzCaJRfIRo=", // Replace with your Redis Cache access key
   tls: { servername: REDIS_HOST }, 
-  //  "myRedisInstance15813d52.redis.cache.windows.net" },
 });
 
 
